10c781567aa88d478a78856ad89920559171e11c
WEBDEVSSC <soportewebssc@gmail.com>
2026-01-26 21:06:27 -0600

    FIX | CONFIG PARA MYSQL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Config::set('database.default', 'mysql');    
         //
         Schema::defaultStringLength(191);
     }
